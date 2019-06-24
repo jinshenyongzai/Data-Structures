1efdf33ce3b85d30468467b21b641f9a9e18d3f1
FD-LJS <1021866202@qq.com>
2019-06-24 10:50:37 +0800

    Add files via upload

---
+++ Queues/Main.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+public class Main {
+
+    //测试使用q运行opCount个enqueue和dequeue操作所需要的时间，单位：秒
+    private static double testQueue(Queue<Integer> q, int opCount) {
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++)
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < opCount; i++)
+            q.dequeue();
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+//        ArrayQueue<Integer> queue = new ArrayQueue<>();
+//        for (int i = 0; i < 10; i++) {
+//            queue.enqueue(i);
+//            System.out.println(queue);
+//
+//            if (i % 3 == 2) {
+//                queue.dequeue();
+//                System.out.println(queue);
+//            }
+//        }
+
+//        LoopQueue<Integer> queue = new LoopQueue<>();
+//        for (int i = 0; i < 10; i++) {
+//            queue.enqueue(i);
+//            System.out.println(queue);
+//
+//            if (i % 3 == 2) {
+//                queue.dequeue();
+//                System.out.println(queue);
+//            }
+//        }
+
+        int opCount = 200000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + "s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + "s");
+    }
+}
