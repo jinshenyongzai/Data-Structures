c32287d16c90c1b3867d4d6a2454606497c11ca0
FD-LJS <1021866202@qq.com>
2019-07-06 13:18:43 +0800

    Add files via upload

--- AVL-Tree/Main.java
+++ AVL-Tree/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
 
@@ -11,7 +10,7 @@ public class Main {
         if(FileOperation.readFile("pride-and-prejudice.txt", words)) {
             System.out.println("Total words: " + words.size());
 
-             Collections.sort(words);
+            // Collections.sort(words);
 
             // Test BST
             long startTime = System.nanoTime();
@@ -33,7 +32,7 @@ public class Main {
             System.out.println("BST: " + time + " s");
 
 
-            // Test AVL Tree
+            // Test AVL
             startTime = System.nanoTime();
 
             AVLTree<String, Integer> avl = new AVLTree<>();
@@ -51,6 +50,25 @@ public class Main {
 
             time = (endTime - startTime) / 1000000000.0;
             System.out.println("AVL: " + time + " s");
+
+            // Test RBTree
+            startTime = System.nanoTime();
+
+            RBTree<String, Integer> rbt = new RBTree<>();
+            for (String word : words) {
+                if (rbt.contains(word))
+                    rbt.set(word, rbt.get(word) + 1);
+                else
+                    rbt.add(word, 1);
+            }
+
+            for(String word: words)
+                rbt.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+            System.out.println("RBTree: " + time + " s");
         }
 
         System.out.println();
